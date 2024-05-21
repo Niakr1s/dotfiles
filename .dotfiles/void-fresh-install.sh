9d090a9600908ad959e8ad10dfcee7c64a430a1e
Niakr1s <pavel2188@gmail.com>
2024-05-21 17:43:22 +0300

    added void-fresh-install.sh

---
+++ .dotfiles/void-fresh-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+repo=https://github.com/Niakr1s/dotfiles
+
+echo
+echo "Repository: $repo."
+echo
+
+echo "This script is intended to configure new, freshly installed void linux system."
+echo
+
+echo "Warning: Before running this script, ensure you've run this commands:"
+echo "sudo xbps-install yadm"
+echo "yadm clone $repo"
+echo
+
+echo "Do you wish to continue?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) exit;;
+    esac
+done
+echo
+
+echo "Updating system..."
+sudo xbps-install -Su
+echo
+
+echo "Installing packages..."
+sudo xargs -a ~/.dotfiles/void-pkgs.txt xbps-install
+echo
+
+echo "Installing oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
+echo

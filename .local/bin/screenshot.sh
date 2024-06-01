e4d9706a8c14af352f7a17c07ac322d6fd7bd693
Niakr1s <pavel2188@gmail.com>
2024-06-01 23:33:36 +0300

    screenshot saves to tmp and to clipboard

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-dir=~/Screenshots
+dir=/tmp/screenshots
 file="$(date +"%Y%m%d_%H%M%S").png"
 
 mkdir -p "$dir"
-maim -s "$dir/$file"
-notify-send "Screenshot saved to $dir/$file" 
+maim -s --hidecursor  | tee "$dir/$file" | xclip -selection clipboard -t image/png
+notify-send "Screenshot saved to clipboard and to $dir/$file" 

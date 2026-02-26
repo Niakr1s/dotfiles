94eb5d98291ba3c94debd2fbb6d182a2806e03f5
Niakr1s <pavel2188@gmail.com>
2026-02-27 00:33:56 +0300

    added proxies

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -10,7 +10,11 @@ set EDITOR vim
 set TERMINAL kitty
 set TERM kitty
 set FZF_DEFAULT_COMMAND "fd --type f --hidden --exclude .git --exclude dosdevices --exclude drive_c"
+
 set v2rayn_proxy "127.0.0.1:10808"
+set http_proxy "http://$v2rayn_proxy"
+set https_proxy "http://$v2rayn_proxy"
+set no_proxy "localhost,127.0.0.1"
 
 alias logout "hyprctl dispatch exit"
 alias cz "chezmoi"

6b801baa63e3e1e245d7350509c8ea8b2d8a5c4e
Niakr1s <pavel2188@gmail.com>
2026-02-26 10:55:58 +0300

    moved env variables back

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -6,6 +6,12 @@ function fish_greeting
     # smth smth
 end
 
+set EDITOR nvim
+set TERMINAL kitty
+set TERM kitty
+set FZF_DEFAULT_COMMAND "fd --type f --hidden --exclude .git --exclude dosdevices --exclude drive_c"
+set v2rayn_proxy "127.0.0.1:10808"
+
 alias vim "nvim"
 alias vimdiff "nvim -d"
 alias logout "hyprctl dispatch exit"
